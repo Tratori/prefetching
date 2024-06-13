93d5fcf4304d2449242d921a4d1fb97e3961f99a
Fabian Mahling <FabianMahling@web.de>
2024-06-13 10:12:19 +0200

    extend benchmark pipeline

--- scripts/delab_benchmark_pipeline.sh
+++ scripts/delab_benchmark_pipeline.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # Execute
-# ./delab_benchmark_pipeline.sh <node_name> <benchmark_name>
+# ./delab_benchmark_pipeline.sh <node_name> <benchmark_executable_name> <run_name>
 
+LOG_FILE="/prefetching/results/${2}/${1}/build_log.txt"
 
-cd /prefetching
+{
+    cd /prefetching
 
-mkdir -p cmake-build-release-${1}
-cd cmake-build-release-${1}
+    mkdir -p cmake-build-release-${1}
+    cd cmake-build-release-${1}
 
-cmake -DCMAKE_BUILD_TYPE=Release ..
+    cmake -DCMAKE_BUILD_TYPE=Release ..
 
-make -j
+    make -j
 
-./benchmark/${2}
+    if ./benchmark/${2}; then
+        echo "success" >&2
+    else
+        echo "error" >&2
+    fi
+} >"$LOG_FILE" 2>&1
--- scripts/delab_run_benchmark.sh
+++ scripts/delab_run_benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Execute
-# ./delab_run_benchmark.sh <benchmark_name>
+# ./delab_run_benchmark.sh <benchmark_executable_name> <run_name>
 # prerequisites:
 #  - https://github.com/Tratori/prefetching @ $HOME directory in DeLab.
 #  - ubuntu22_04.sqsh in $HOME directory.
@@ -29,8 +29,8 @@ partitions["cp01"]="alchemy"
 nodenames["cp01"]="cp01"
 
 # Check if benchmark with same name was already executed.
-if [ -d "$HOME/prefetching/results/$1" ]; then
-    echo "Benchmark with name $1 was already executed!"
+if [ -d "$HOME/prefetching/results/${2}" ]; then
+    echo "Benchmark-Run with name ${2} was already executed!"
     exit 1
 fi
 
@@ -38,16 +38,20 @@ cd ${HOME}/prefetching
 git reset --hard
 git pull
 
+
+mkdir -p $HOME/prefetching/results/${2}
+git rev-parse HEAD > $HOME/prefetching/results/${2}
+
 for node_conf in ${node_config[@]}; do
     node=${nodenames[$node_conf]}
-    RESULT_FILE="$HOME/prefetching/results/$1/$node_conf"
+    RESULT_FILE="$HOME/prefetching/results/${2}/$node_conf"
 
 
-    mkdir -p $HOME/prefetching/results/$1/$node_conf
+    mkdir -p $HOME/prefetching/results/${2}/$node_conf
 
     echo "submitting task for config ${node_conf}"
     srun -A rabl --partition ${partitions[$node_conf]} -w $node -c 16 --mem-per-cpu 1024 \
       --time=6:00:00 --container-image=${HOME}/ubuntu22_04.sqsh \
       --container-mounts=${HOME}/prefetching:/prefetching  \
-      /prefetching/scripts/delab_benchmark_pipeline.sh ${node_conf} $1  &
+      /prefetching/scripts/delab_benchmark_pipeline.sh ${node_conf} ${1} ${2}  &
 done
