e4c4ba69952ab68c0659dd77856e36b53de7f6a9
Fabian Mahling <FabianMahling@web.de>
2024-06-18 14:41:49 +0200

    some more jemalloc and boost stuff

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-sudo apt-get install --no-install-recommends -y autoconf cmake gcc g++ clang libnuma-dev libnuma1 libboost-all-dev libnuma
+sudo apt-get install --no-install-recommends -y autoconf cmake gcc g++ clang libnuma-dev libnuma1 libboost-all-dev libnuma libboost-all-dev
