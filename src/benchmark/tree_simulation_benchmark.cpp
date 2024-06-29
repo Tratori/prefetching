7c6c9dea41ae2f729e70303dfdc11435066603ff
Fabian Mahling <FabianMahling@web.de>
2024-06-29 17:22:21 +0200

    some more code, semi working scheduler

--- src/benchmark/tree_simulation_benchmark.cpp
+++ src/benchmark/tree_simulation_benchmark.cpp
@@ -4,6 +4,8 @@
 #include <functional>
 #include <chrono>
 #include <assert.h>
+#include <numa.h>
+#include <numaif.h>
 #include <thread>
 #include <iostream>
 #include <fstream>
@@ -16,7 +18,84 @@
 #include "utils/utils.cpp"
 #include "coroutine.hpp"
 
-unsigned find_in_node(uint32_t *node, uint32_t k, uint32_t values_per_node)
+thread_local size_t curr_group_node_id = 9999;
+
+void pin_to_cpu(NodeID cpu)
+{
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(cpu, &cpuset);
+    const auto return_code = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
+    if (return_code != 0)
+    {
+        throw std::runtime_error("pinning thread to cpu failed (return code: " + std::to_string(return_code) + ").");
+    }
+}
+
+/*
+    TODO: run on SMT (aka all logical cores on a physical core)
+    We run the schedulers in groups, each group containing exactly one core per NUMA node.
+    Each scheduler "owns" a thread_frame where it stores coroutines that currently run on this thread.
+    Furthermore, scheduler 1 owns the first <coroutine_n> slots, scheduler 2 the next n slots, making sure
+    new work is pushed when coroutines are finished.
+*/
+struct thread_frame // TODO: align this struct to Cache line
+{
+    std::atomic<uint8_t> *running_coroutines = nullptr;
+    std::atomic<task *> *coroutines = nullptr;
+    thread_frame(size_t number_of_coroutines)
+    {
+        running_coroutines = new std::atomic<uint8_t>[number_of_coroutines];
+        coroutines = new std::atomic<task *>[number_of_coroutines];
+    }
+    ~thread_frame()
+    {
+        delete[] running_coroutines;
+        delete[] coroutines;
+    }
+};
+
+template <typename handle>
+auto jump_between_threads(std::vector<thread_frame *> &thread_frames, NodeID from, NodeID to, int coroutine_id)
+{
+    struct awaitable
+    {
+        std::vector<thread_frame *> &thread_frames;
+        NodeID from;
+        NodeID to;
+        int coroutine_id;
+        bool await_ready() { return false; }
+        void await_suspend(handle h)
+        {
+            thread_frame *from_node = thread_frames[from];
+            thread_frame *to_node = thread_frames[to];
+            from_node->running_coroutines[coroutine_id] = 0;
+            from_node->coroutines[coroutine_id] = nullptr;
+            to_node->coroutines[coroutine_id] = h;
+
+            // make sure this is the last thing that happens
+            // thread can instantly pick up. Perhaps mem barriers
+            // should be used.
+            to_node->running_coroutines[coroutine_id] = 1;
+        }
+        void await_resume() {}
+    };
+    return awaitable{thread_frames, from, to, coroutine_id};
+}
+
+struct TreeSimulationConfig
+{
+    size_t tree_node_size;
+    NodeID numa_nodes;
+    size_t memory_per_node;
+    size_t num_threads;
+    size_t coroutines;
+    size_t num_lookups;
+    size_t num_node_traversal_per_lookup;
+};
+
+unsigned
+find_in_node(uint32_t *node, uint32_t k, uint32_t values_per_node)
 {
     unsigned lower = 0;
     unsigned upper = values_per_node;
@@ -67,34 +146,148 @@ task co_find_in_node(uint32_t *node, uint32_t k, uint32_t values_per_node, uint3
     co_return;
 }
 
-task co_tree_traversal(size_t num_node_traversal_per_lookup, char *data, uint32_t k,
-                       uint32_t values_per_node, size_t tree_node_size,
+task co_tree_traversal(TreeSimulationConfig &config, char *data, uint32_t k, uint32_t values_per_node,
                        std::uniform_int_distribution<> node_distribution, auto gen)
 {
     int sum = 0;
-    for (int j = 0; j < num_node_traversal_per_lookup; j++)
+    for (int j = 0; j < config.num_node_traversal_per_lookup; j++)
     {
         auto next_node = node_distribution(gen);
         uint32_t found;
-        co_find_in_node(reinterpret_cast<uint32_t *>(data + (next_node * tree_node_size)), k, values_per_node, found);
+        co_find_in_node(reinterpret_cast<uint32_t *>(data + (next_node * config.tree_node_size)), k, values_per_node, found);
         sum += found;
     }
-    if (sum != num_node_traversal_per_lookup * k)
+    if (sum != config.num_node_traversal_per_lookup * k)
     {
-        "lookups failed " + std::to_string(sum) + " vs. " + std::to_string(num_node_traversal_per_lookup * k);
+        "lookups failed " + std::to_string(sum) + " vs. " + std::to_string(config.num_node_traversal_per_lookup * k);
     }
     co_return;
 }
 
-void tree_simulation_coroutine(size_t repetitions, size_t group_size, size_t values_per_node,
-                               size_t num_tree_nodes, size_t tree_node_size,
-                               size_t num_node_traversal_per_lookup, char *data)
+void scheduler_thread_function(NodeID cpu, std::vector<std::atomic<thread_frame *>> &thread_frames, char *data, size_t group_thread_id, size_t values_per_node,
+                               size_t num_tree_nodes, std::atomic<size_t> &finished, TreeSimulationConfig config)
+{
+    pin_to_cpu(cpu);
+
+    curr_group_node_id = group_thread_id;
+    auto num_total_coroutine_slots = config.coroutines * config.numa_nodes;
+    auto tf = new thread_frame{num_total_coroutine_slots};
+    thread_frames[group_thread_id] = tf;
+
+    size_t num_finished = 0;
+    size_t num_scheduled = 0;
+    auto repetitions = config.num_lookups / config.num_threads;
+    auto tf_start_slot = config.coroutines * group_thread_id;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> uniform_dis_node_value(0, values_per_node - 1);
+    std::uniform_int_distribution<> uniform_dis_next_node(0, num_tree_nodes - 1);
+
+    while (true)
+    {
+        for (size_t i = 0; i < num_total_coroutine_slots; ++i)
+        {
+            if (tf_start_slot <= i && i < (tf_start_slot + config.coroutines))
+            {
+                switch (tf->running_coroutines[i])
+                {
+                case 0: // empty, insert new
+                {
+                    if (num_scheduled == repetitions)
+                    {
+                        tf->running_coroutines[i] = 2;
+                        continue;
+                    }
+                    auto k = uniform_dis_node_value(gen);
+                    tf->coroutines[i] = new task(co_tree_traversal(config, data, k, values_per_node,
+                                                                   uniform_dis_next_node, gen));
+                    num_scheduled++;
+                    tf->coroutines[i].load()->coro.resume();
+                    tf->running_coroutines[i] = 1;
+                }
+                break;
+                case 1:
+                    if (!tf->coroutines[i].load()->coro.done())
+                    {
+                        tf->coroutines[i].load()->coro.resume();
+                    }
+                    else
+                    {
+                        delete tf->coroutines[i];
+                        tf->coroutines[i] = nullptr;
+                        tf->running_coroutines[i] = 0;
+                        num_finished++;
+                        if (num_finished == repetitions)
+                        {
+                            finished++;
+                        }
+                        if (num_finished > repetitions)
+                        {
+                            throw std::runtime_error("Num_finished > repetitions.");
+                        }
+                    }
+                    break;
+                case 2: // finished scheduling, ignore
+                    break;
+                default:
+                    throw std::runtime_error("unknown state in running coroutines");
+                    break;
+                }
+            }
+            else
+            {
+                // Handle other coroutines if necessary
+                if (tf->running_coroutines[i] > 0)
+                {
+                    if (!tf->coroutines[i].load()->coro.done())
+                    {
+                        tf->coroutines[i].load()->coro.resume();
+                    }
+                    else
+                    {
+                        throw std::runtime_error("Coroutine finished on foreign node");
+                    }
+                }
+            }
+        }
+        if (num_finished == repetitions)
+        {
+            if (finished.load() == config.numa_nodes)
+            {
+                return;
+            }
+        }
+    }
+}
+
+void initialize_scheduler_groups(char *data, std::vector<NodeID> cpus, size_t values_per_node, size_t num_tree_nodes, TreeSimulationConfig config)
+{
+    std::vector<std::jthread> threads;
+    std::vector<std::atomic<thread_frame *>> thread_frames(cpus.size());
+    for (int i = 0; i < cpus.size(); ++i)
+    {
+        thread_frames[i] = nullptr;
+    }
+    std::atomic<size_t> finished = 0;
+    for (int i = 1; i < cpus.size(); ++i)
+    {
+        threads.emplace_back(scheduler_thread_function, cpus[i], std::ref(thread_frames), data, i, values_per_node, num_tree_nodes, std::ref(finished), config);
+    }
+    scheduler_thread_function(cpus[0], thread_frames, data, 0, values_per_node, num_tree_nodes, std::ref(finished), config); // this thread also works as a scheduler;
+    for (auto &t : threads)
+    {
+        t.join();
+    }
+}
+
+void tree_simulation_coroutine(TreeSimulationConfig config, size_t repetitions, size_t values_per_node, size_t num_tree_nodes, char *data)
 {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> uniform_dis_node_value(0, values_per_node - 1);
     std::uniform_int_distribution<> uniform_dis_next_node(0, num_tree_nodes - 1);
-    CircularBuffer<task> buff(group_size);
+    CircularBuffer<task> buff(config.coroutines);
 
     size_t num_finished = 0;
     int num_scheduled = 0;
@@ -107,11 +300,11 @@ void tree_simulation_coroutine(size_t repetitions, size_t group_size, size_t val
             if (num_scheduled < repetitions)
             {
                 auto k = uniform_dis_node_value(gen);
-                handle = co_tree_traversal(num_node_traversal_per_lookup, data, k, values_per_node, tree_node_size,
+                handle = co_tree_traversal(config, data, k, values_per_node,
                                            uniform_dis_next_node, gen);
                 num_scheduled++;
             }
-            if (num_scheduled > group_size)
+            if (num_scheduled > config.coroutines)
             {
                 num_finished++;
             }
@@ -122,29 +315,26 @@ void tree_simulation_coroutine(size_t repetitions, size_t group_size, size_t val
     }
 }
 
-void benchmark_tree_simulation(size_t tree_node_size, NodeID numa_nodes,
-                               size_t memory_per_node, size_t num_threads,
-                               size_t coroutines, size_t num_lookups,
-                               size_t num_node_traversal_per_lookup)
+void benchmark_tree_simulation(TreeSimulationConfig &config)
 {
 
-    InterleavingNumaMemoryResource mem_res{numa_nodes};
-    auto total_memory = memory_per_node * 1024 * 1024 * numa_nodes; // memory given in MiB
+    InterleavingNumaMemoryResource mem_res{config.numa_nodes};
+    auto total_memory = config.memory_per_node * 1024 * 1024 * config.numa_nodes; // memory given in MiB
     std::pmr::vector<char> data(total_memory, &mem_res);
 
-    auto num_tree_nodes = total_memory / tree_node_size;
-    auto values_per_node = tree_node_size / sizeof(u_int32_t); // we use 4B "keys"
+    auto num_tree_nodes = total_memory / config.tree_node_size;
+    auto values_per_node = config.tree_node_size / sizeof(u_int32_t); // we use 4B "keys"
     for (size_t i = 0; i < num_tree_nodes; i++)
     {
         for (uint32_t j = 0; j < values_per_node; ++j)
         {
-            *(reinterpret_cast<uint32_t *>(data.data() + (tree_node_size * i)) + j) = j;
+            *(reinterpret_cast<uint32_t *>(data.data() + (config.tree_node_size * i)) + j) = j;
         }
     }
 
     auto start = std::chrono::high_resolution_clock::now();
     std::vector<std::jthread> threads;
-    for (size_t t = 0; t < num_threads; ++t)
+    for (size_t t = 0; t < config.num_threads; ++t)
     {
         threads.emplace_back([&]()
                              {
@@ -152,20 +342,23 @@ void benchmark_tree_simulation(size_t tree_node_size, NodeID numa_nodes,
                                   std::mt19937 gen(rd());
                                   std::uniform_int_distribution<> uniform_dis_node_value(0, values_per_node-1);
                                   std::uniform_int_distribution<> uniform_dis_next_node(0, num_tree_nodes-1);
-                                  for (size_t i = 0; i < num_lookups / num_threads; ++i){
+                                  for (size_t i = 0; i < config.num_lookups / config.num_threads; ++i)
+                                  {
                                       int next_node;
                                       int searched_value = uniform_dis_node_value(gen);
                                       auto test_counter = 0;
-                                      for(size_t j= 0; j < num_node_traversal_per_lookup; ++j){
+                                      for (size_t j = 0; j < config.num_node_traversal_per_lookup; ++j)
+                                      {
                                           next_node =  uniform_dis_next_node(gen);
-                                          test_counter += find_in_node(reinterpret_cast<uint32_t*>(data.data() + (tree_node_size * next_node)), searched_value, values_per_node);
+                                          test_counter += find_in_node(reinterpret_cast<uint32_t *>(data.data() + (config.tree_node_size * next_node)), searched_value, values_per_node);
                                       }
-                                      if(test_counter != num_node_traversal_per_lookup * searched_value){
-                                          throw std::runtime_error("lookups failed " + std::to_string(test_counter) + " vs. " + std::to_string(num_node_traversal_per_lookup * searched_value));
+                                      if (test_counter != config.num_node_traversal_per_lookup * searched_value)
+                                      {
+                                          throw std::runtime_error("lookups failed " + std::to_string(test_counter) + " vs. " + std::to_string(config.num_node_traversal_per_lookup * searched_value));
                                       }
                                   } });
     }
-    for (size_t t = 0; t < num_threads; ++t)
+    for (size_t t = 0; t < config.num_threads; ++t)
     {
         threads[t].join();
     }
@@ -174,12 +367,31 @@ void benchmark_tree_simulation(size_t tree_node_size, NodeID numa_nodes,
 
     start = std::chrono::high_resolution_clock::now();
     threads.clear();
-    for (size_t t = 0; t < num_threads; ++t)
+    for (size_t t = 0; t < config.num_threads; ++t)
     {
-        threads.emplace_back(tree_simulation_coroutine, num_lookups / num_threads, coroutines, values_per_node,
-                             num_tree_nodes, tree_node_size, num_node_traversal_per_lookup, data.data());
+        threads.emplace_back(tree_simulation_coroutine, config, config.num_lookups / config.num_threads, values_per_node,
+                             num_tree_nodes, data.data());
     }
-    for (size_t t = 0; t < num_threads; ++t)
+    for (size_t t = 0; t < config.num_threads; ++t)
+    {
+        threads[t].join();
+    }
+    end = std::chrono::high_resolution_clock::now();
+    std::cout << "multithreaded coroutine lookup took: " << std::chrono::duration<double>(end - start).count() << " seconds" << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    threads.clear();
+    auto node_2_cpus = Prefetching::get().numa_manager.node_to_cpus;
+    for (size_t t = 0; t < config.num_threads / config.numa_nodes; ++t) // We effectively schedule config.numa_nodes threads per run here.
+    {
+        std::vector<NodeID> cpus(config.numa_nodes, 0);
+        for (int i = 0; i < config.numa_nodes; ++i)
+        {
+            cpus[i] = node_2_cpus[i][t];
+        }
+        threads.emplace_back(initialize_scheduler_groups, data.data(), cpus, values_per_node, num_tree_nodes, config);
+    }
+    for (size_t t = 0; t < config.num_threads; ++t)
     {
         threads[t].join();
     }
@@ -213,10 +425,10 @@ int main(int argc, char **argv)
         auto coroutines = convert<size_t>(runtime_config["coroutines"]);
         auto num_lookups = convert<size_t>(runtime_config["num_lookups"]);
         auto num_node_traversal_per_lookup = convert<size_t>(runtime_config["num_node_traversal_per_lookup"]);
-
+        TreeSimulationConfig config = {tree_node_size, numa_nodes, memory_per_node, num_threads, coroutines, num_lookups, num_node_traversal_per_lookup};
         nlohmann::json results;
 
-        benchmark_tree_simulation(tree_node_size, numa_nodes, memory_per_node, num_threads, coroutines, num_lookups, num_node_traversal_per_lookup);
+        benchmark_tree_simulation(config);
         auto results_file = std::ofstream{"tree_simulation_" + std::to_string(benchmark_run++) + ".json"};
         results_file << results.dump(-1) << std::flush;
     }
