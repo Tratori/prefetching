6d0986c5d09740f62344ef999b9b3dd2d7513fe4
Fabian Mahling <FabianMahling@web.de>
2024-07-02 17:36:08 +0200

    add batched lfb variant

--- src/benchmark/lfb_size_benchmark.cpp
+++ src/benchmark/lfb_size_benchmark.cpp
@@ -19,9 +19,65 @@ struct LFBBenchmarkConfig
     size_t total_memory;
     size_t num_threads;
     size_t num_repetitions;
-    size_t num_prefetch_instructions;
+    size_t batch_size;
     size_t prefetch_distance;
     bool use_huge_pages;
+};
+
+void simple_interleaved(size_t i, size_t number_accesses, auto &config, auto &data, auto &accesses, auto &durations)
+{
+    pin_to_cpu(Prefetching::get().numa_manager.node_to_cpus[0][i]);
+    size_t start_access = i * number_accesses;
+    size_t sum = 0;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (size_t j = 0; j < number_accesses; ++j)
+    {
+        __builtin_prefetch(reinterpret_cast<void *>(data.data() + accesses[j]), 0, 3);
+        if (j > config.prefetch_distance)
+        {
+            sum += *reinterpret_cast<uint8_t *>(data.data() + accesses[j - config.prefetch_distance]);
+        }
+    }
+    for (size_t j = config.prefetch_distance; j > 0; --j)
+    {
+        sum += *reinterpret_cast<uint8_t *>(data.data() + accesses[number_accesses - j]);
+    }
+    if (sum == 0)
+    {
+        throw std::runtime_error("sum not correct");
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    durations[i] = std::chrono::duration<double>(end - start);
+};
+
+void batched_load(size_t i, size_t number_accesses, auto &config, auto &data, auto &accesses, auto &durations)
+{
+    pin_to_cpu(Prefetching::get().numa_manager.node_to_cpus[0][i]);
+    size_t start_access = i * number_accesses;
+    size_t sum = 0;
+
+    auto num_batches = number_accesses / config.batch_size;
+    auto start = std::chrono::high_resolution_clock::now();
+    for (size_t b = 0; b < num_batches; ++b)
+    {
+        auto offset = b * config.batch_size;
+        for (size_t i = 0; i < config.batch_size; ++i)
+        {
+            __builtin_prefetch(reinterpret_cast<void *>(data.data() + accesses[offset + i]), 0, 3);
+        }
+        for (size_t i = 0; i < config.batch_size; ++i)
+        {
+            sum += *reinterpret_cast<uint8_t *>(data.data() + accesses[offset + i]);
+        }
+        _mm_lfence();
+        if (sum == 0)
+        {
+            throw std::runtime_error("sum not correct");
+        }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    durations[i] = std::chrono::duration<double>(end - start);
 };
 
 void lfb_size_benchmark(LFBBenchmarkConfig config, nlohmann::json &results)
@@ -52,26 +108,7 @@ void lfb_size_benchmark(LFBBenchmarkConfig config, nlohmann::json &results)
     for (size_t i = 0; i < config.num_threads; ++i)
     {
         threads.emplace_back([&, i]()
-                             {
-            pin_to_cpu(Prefetching::get().numa_manager.node_to_cpus[0][i]);
-            size_t start_access = i * number_accesses;
-            size_t sum = 0;
-            auto start = std::chrono::high_resolution_clock::now();
-
-            for(size_t j = 0; j < number_accesses; ++j){
-                __builtin_prefetch(reinterpret_cast<void*>(data.data() + accesses[j]), 0, 3);
-                if(j > config.prefetch_distance){
-                    sum += *reinterpret_cast<uint8_t*>(data.data() + accesses[j-config.prefetch_distance]);
-                }
-            }
-            for(size_t j = config.prefetch_distance; j > 0; --j){
-                sum += *reinterpret_cast<uint8_t*>(data.data() + accesses[number_accesses-j]);
-            }
-            if(sum == 0){
-                throw std::runtime_error("sum not correct");
-            }
-            auto end = std::chrono::high_resolution_clock::now();
-            durations[i] = std::chrono::duration<double>(end - start); });
+                             { batched_load(i, number_accesses, config, data, accesses, durations); });
     }
     for (auto &t : threads)
     {
@@ -95,7 +132,7 @@ int main(int argc, char **argv)
         ("total_memory", "Total memory allocated MiB", cxxopts::value<std::vector<size_t>>()->default_value("2048"))
         ("num_threads", "Number of threads running the bench", cxxopts::value<std::vector<size_t>>()->default_value("8"))
         ("num_repetitions", "Number of repetitions of the measurement", cxxopts::value<std::vector<size_t>>()->default_value("10000000"))
-        ("num_prefetch_instructions", "Number of distinct prefetch instructions issued per measurement", cxxopts::value<std::vector<size_t>>()->default_value("10"))
+        ("batch_size", "Number of distinct prefetch instructions before loads start", cxxopts::value<std::vector<size_t>>()->default_value("10"))
         ("prefetch_distance", "number of prefetches between corresponding prefetch and load", cxxopts::value<std::vector<size_t>>()->default_value("10"))
         ("use_huge_pages", "Use huge pages during allocation", cxxopts::value<std::vector<bool>>()->default_value("true"));
     // clang-format on
@@ -109,14 +146,14 @@ int main(int argc, char **argv)
         auto total_memory = convert<size_t>(runtime_config["total_memory"]);
         auto num_threads = convert<size_t>(runtime_config["num_threads"]);
         auto num_repetitions = convert<size_t>(runtime_config["num_repetitions"]);
-        auto num_prefetch_instructions = convert<size_t>(runtime_config["num_prefetch_instructions"]);
+        auto batch_size = convert<size_t>(runtime_config["batch_size"]);
         auto prefetch_distance = convert<size_t>(runtime_config["prefetch_distance"]);
         auto use_huge_pages = convert<bool>(runtime_config["use_huge_pages"]);
         LFBBenchmarkConfig config = {
             total_memory,
             num_threads,
             num_repetitions,
-            num_prefetch_instructions,
+            batch_size,
             prefetch_distance,
             use_huge_pages,
         };
@@ -124,7 +161,7 @@ int main(int argc, char **argv)
         results["config"]["total_memory"] = config.total_memory;
         results["config"]["num_threads"] = config.num_threads;
         results["config"]["num_repetitions"] = config.num_repetitions;
-        results["config"]["num_prefetch_instructions"] = config.num_prefetch_instructions;
+        results["config"]["batch_size"] = config.batch_size;
         results["config"]["prefetch_distance"] = config.prefetch_distance;
         results["config"]["use_huge_pages"] = config.use_huge_pages;
 
--- visualization/lfb_size.py
+++ visualization/lfb_size.py
@@ -8,17 +8,18 @@ from helper import *
 
 
 ids = ["amd", "intel", "local"]
-filenames = {
+filenames_interleaving = {
     "amd": "lfb_size_simple_interleaving_amd.json",
     "intel": "lfb_size_simple_interleaving_intel.json",
     "local": "lfb_size_simple_interleaving_local.json",
 }
 
-if __name__ == "__main__":
 
+def plot_interleaved():
     for id in ids:
-
-        lfb_bench = import_benchmark(filenames[id])
+        lfb_bench = import_benchmark(
+            os.path.join("lfb_interleaved", filenames_interleaving[id])
+        )
         results = lfb_bench["results"]
         results = sorted(
             results, key=lambda result: result["config"]["prefetch_distance"]
@@ -29,8 +30,36 @@ if __name__ == "__main__":
         runtime = [result["runtime"] for result in results]
 
         sns.pointplot(x=prefetching_distances, y=runtime)
-    plt.title("LFB Size Benchmark")
+    plt.title("LFB Size Benchmark - Interleaved")
     plt.xlabel("Prefetch Distance")
     plt.ylabel("Runtime (s)")
 
     plt.show()
+
+
+ids = ["amd", "intel", "local"]
+filenames_batched = {
+    "amd": "lfb_size_batched_amd.json",
+    "intel": "lfb_size_batched_intel.json",
+    "local": "lfb_size_batched_local.json",
+}
+
+
+def plot_batched():
+    for id in ["local"]:
+        lfb_bench = import_benchmark(os.path.join("lfb_batched", filenames_batched[id]))
+        results = lfb_bench["results"]
+        results = sorted(results, key=lambda result: result["config"]["batch_size"])
+        prefetching_distances = [result["config"]["batch_size"] for result in results]
+        runtime = [result["runtime"] for result in results]
+
+        sns.pointplot(x=prefetching_distances, y=runtime)
+    plt.title("LFB Size Benchmark - Batched")
+    plt.xlabel("Batch Size")
+    plt.ylabel("Runtime (s)")
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot_interleaved()
+    plot_batched()
