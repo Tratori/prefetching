bf1c51e5a43b4a50841913be3b53b90e5fffb0f8
Fabian Mahling <FabianMahling@web.de>
2024-07-03 10:32:05 +0200

    some first batched bench results

--- visualization/lfb_size.py
+++ visualization/lfb_size.py
@@ -13,7 +13,11 @@ filenames_interleaving = {
     "intel": "lfb_size_simple_interleaving_intel.json",
     "local": "lfb_size_simple_interleaving_local.json",
 }
-
+lfbs = {
+    "amd": 22.0,
+    "intel": 10.0,
+    "local": 22.0,
+}
 
 def plot_interleaved():
     for id in ids:
@@ -25,11 +29,14 @@ def plot_interleaved():
             results, key=lambda result: result["config"]["prefetch_distance"]
         )
         prefetching_distances = [
-            result["config"]["prefetch_distance"] for result in results
+            int(result["config"]["prefetch_distance"]) for result in results
         ]
         runtime = [result["runtime"] for result in results]
 
-        sns.pointplot(x=prefetching_distances, y=runtime)
+        pointplot = sns.pointplot(x=prefetching_distances, y=runtime, label=id)
+        # color = pointplot.get_lines()[0].get_color()  # Get the color of the pointplot
+        # sns.lineplot(x=[10, 10], y=[0, 0.3], color=color)
+
     plt.title("LFB Size Benchmark - Interleaved")
     plt.xlabel("Prefetch Distance")
     plt.ylabel("Runtime (s)")
@@ -46,17 +53,19 @@ filenames_batched = {
 
 
 def plot_batched():
-    for id in ["local"]:
+    for id in ids:
         lfb_bench = import_benchmark(os.path.join("lfb_batched", filenames_batched[id]))
         results = lfb_bench["results"]
         results = sorted(results, key=lambda result: result["config"]["batch_size"])
         prefetching_distances = [result["config"]["batch_size"] for result in results]
         runtime = [result["runtime"] for result in results]
+        runtime = [p[0] * p[1] for p in zip(runtime, prefetching_distances)]
 
-        sns.pointplot(x=prefetching_distances, y=runtime)
+        sns.pointplot(x=prefetching_distances, y=runtime, label=id)
     plt.title("LFB Size Benchmark - Batched")
     plt.xlabel("Batch Size")
     plt.ylabel("Runtime (s)")
+    # plt.yscale("log")
     plt.show()
 
 
