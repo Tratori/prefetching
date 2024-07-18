35b8678b253e7396491e1a164af4b7256163539e
Fabian Mahling <FabianMahling@web.de>
2024-07-18 10:35:06 +0200

    limit runtime even less as a precaution

--- scripts/delab_run_benchmark.sh
+++ scripts/delab_run_benchmark.sh
@@ -64,7 +64,7 @@ for node_conf in ${node_config[@]}; do
 
     echo "submitting task for config ${node_conf}"
     srun -A rabl --partition ${partitions[$node_conf]} -w $node -c ${num_cpus[$node_conf]} \
-      --time=24:00:00 --container-image=${HOME}/ubuntu22_04.sqsh \
+      --time=36:00:00 --container-image=${HOME}/ubuntu22_04.sqsh \
       --container-mounts=${HOME}/prefetching:/prefetching  \
       /prefetching/scripts/delab_benchmark_pipeline.sh ${node_conf} ${1} ${2} ${@:3} &
 done
