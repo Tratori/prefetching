0bf91944dba36b555db31984dc545d390d96427b
Fabian Mahling <FabianMahling@web.de>
2024-07-31 12:09:24 +0200

    split setup into required and helpful tools

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-sudo apt-get install --no-install-recommends -y autoconf cmake make gcc g++ clang libnuma-dev libnuma1 libboost-all-dev libnuma libboost-all-dev git nano
+# Required:
+sudo apt-get install --no-install-recommends -y autoconf cmake make gcc g++ clang libnuma-dev libnuma1 libboost-all-dev libnuma libboost-all-dev
+# Helpful tools:
+sudo apt-get install --no-install-recommends -y git nano gdb
