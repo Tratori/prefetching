2453a9cd35780f53fa1a1a14828885c0477bbeb3
Fabian Mahling <FabianMahling@web.de>
2024-08-08 14:34:09 +0200

    fail on make / cmake failure

--- scripts/delab_benchmark_pipeline.sh
+++ scripts/delab_benchmark_pipeline.sh
@@ -12,8 +12,16 @@ RESULT_FILE="/prefetching/results/${3}/${1}/${2}.json"
     cd cmake-build-release-${1}
 
     cmake -DCMAKE_BUILD_TYPE=Release ..
+    if [ $? -ne 0 ]; then
+        echo "cmake command failed" >&2
+        exit 1
+    fi
 
     make -j
+    if [ $? -ne 0 ]; then
+        echo "make command failed" >&2
+        exit 1
+    fi
 
     if ./benchmark/${2} --out="$RESULT_FILE" ${@:4}; then
         echo "success" >&2
